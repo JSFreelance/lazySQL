2c37c2b23ef6cc05e6bffe4cb21e01e9d6b9658b
Jairo <jairosxd1992@gmail.com>
2016-02-17 17:32:07 +0100

    First commit, select needs to be finished

---
+++ lazySQL/Connection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
+
+namespace lazySQL
+{
+    class Connection<T>
+    {
+        string host = ConfigurationSettings.AppSettings["host"];
+        string user = ConfigurationSettings.AppSettings["user"];
+        string password = ConfigurationSettings.AppSettings["password"];
+        string db = ConfigurationSettings.AppSettings["db"];
+        StringBuilder connString = new StringBuilder();
+        resultSet<T> rs;
+        List<T> row;
+
+        public Connection()
+        {
+            connString.Append(host);
+            connString.Append(user);
+            connString.Append(password);
+            connString.Append(db);
+        }
+
+        public resultSet<T> getSelect(string queryStr)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(connString.ToString()))
+            {
+                conn.Open();
+                NpgsqlCommand command = new NpgsqlCommand(queryStr, conn);
+                NpgsqlDataReader dataReader = command.ExecuteReader();
+
+                //get Num columns:
+                int nColumns = dataReader.FieldCount;
+                rs = new resultSet<T>();
+
+                while (dataReader.Read())
+                {
+                    row = new List<T>();
+                    for (int i = 0; i < nColumns; i++)
+                    {
+                        row.Add((T)dataReader[i]);
+                    }
+                    rs.RowList.Add(row);
+                }
+            }
+            return rs;
+
+        }
+    }
+}
---
+++ lazySQL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Npgsql;
+
+namespace lazySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Output rows
+            /*
+             while (dr.Read())
+                 Console.Write("{0}\t{1} \n", dr[0], dr[1]);
+
+             String query = "SELECT * FROM users";
+             
+             Connection<Object> c = new Connection<Object>();
+             resultSet<Object> rs = c.getSelect(query);
+
+            foreach (List<Object> ls in rs.RowList)
+            {
+                foreach (Object i in ls)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }*/
+
+            Query q = new Query();
+            q.Select("id", "name").from("users");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
